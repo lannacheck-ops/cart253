61315752e5aeaa5c0d87320eb04c0e3c6cec5955
Lanna Check <lannacheck@gmail.com>
2025-09-10 00:38:48 -0400

    Added comments and a tittle
    Making the code more readable

--- Instructions/drawing-practice/template-p5-project/js/script.js
+++ Instructions/drawing-practice/template-p5-project/js/script.js
@@ -1,15 +1,14 @@
 /**
- * Bear Drawing Practice
+ * Cute Bear Drawing Practice
  * Lanna Check
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Just a cute bear staring at the viewer :)
  */
 
 "use strict";
 
 /** 
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a canvas
 */
 function setup() {
 createCanvas(640, 640);
@@ -17,43 +16,57 @@ createCanvas(640, 640);
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the face of a cute bear
 */
 function draw() {
+    // a yellow background
   background("#f6ee76ff");
 
   push();
   noStroke();
   fill("#705332ff");
+  //The head of a brown bear
   ellipse(320, 320, 400);
+  //The left ear of the bear
   ellipse(220, 160, 140);
+  //The right ear of the bear
   ellipse(420, 160, 140);
   pop();
 
+//The eyes
   push();
   stroke(0);
   strokeWeight(40);
+  //left eye
   point(250, 300);
+  //right eye
   point(390, 300);
   pop();
 
+//Eye shine
   push();
   stroke(255);
   strokeWeight(10);
+  //left eye shine
   point(250, 290);
+  //right eye shine
   point(390, 290);
   pop();
 
+  //The nose
   push();
   noStroke();
   fill(0);
   triangle(300, 350, 340, 350, 320, 370);
   pop();
 
+//The mouth or lips
   push();
   stroke(0);
   strokeWeight(8);
+  //left lip
   line(318, 368, 290, 400);
+  //right lip
   line(321, 368, 349, 400);
   pop();
 }
