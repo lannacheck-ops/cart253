5a2c7420b98a3f0b739654ee9783129f05cb4b08
Lanna Check <lannacheck@gmail.com>
2025-09-09 23:40:12 -0400

    Art piece that represents the anxiety of looking back into the past
    Added two rectangles: a black one and a white one to represent an eye looking backwards. The eye looks back into the past and frowns (the black line being the eyebrow).

--- Instructions/function-calls/template-p5-project/js/script.js
+++ Instructions/function-calls/template-p5-project/js/script.js
@@ -16,6 +16,17 @@ createCanvas(640,480);
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-background(227, 180, 100);
-rect(200, 200, 100, 80);
+    //background represents the face of the character
+    background(227, 180, 100);
+    //the dark area of the character's eye
+    fill(0);
+    rect(200, 200, 100, 80);
+    //the white area of the character's eye
+    fill(255);
+    rect(200, 210, 65, 60);
+    //the character's eyebrow
+    strokeWeight(10)
+    line(300, 100, 198, 130);
+    
+    
 }
