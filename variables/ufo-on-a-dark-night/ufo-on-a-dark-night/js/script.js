bafec99ced515f8b909d3d27b75ff8f5ea2df875
Lanna Check <lannacheck@gmail.com>
2025-09-15 02:04:08 -0400

    Used variables to make the UFO fly away

--- variables/ufo-on-a-dark-night/ufo-on-a-dark-night/js/script.js
+++ variables/ufo-on-a-dark-night/ufo-on-a-dark-night/js/script.js
@@ -33,8 +33,23 @@ function setup() {
  * Displays a UFO
 */
 function draw() {
+    // Dawn
+    skyShade = skyShade + 1;
     // Display the sky
     background(skyShade);
+
+    // Move the UFO
+    ufo.x = ufo.x + 0.5;
+    ufo.y = ufo.y - 2;
+
+    // Change ufo's shape
+    ufo.width = ufo.width / 1.005;
+    ufo.height = ufo.height / 1.005;
+
+
+    // Change ufo's fill
+    ufo.fill = ufo.fill * 0.995;
+
 
     // Draw the UFO based on its properties
     push();
