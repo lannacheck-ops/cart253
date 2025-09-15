b6cc05e36604a8e8e87b0ac5b7e1e6d9b4ef7899
Lanna Check <lannacheck@gmail.com>
2025-09-15 01:11:18 -0400

    Started the creating variables module

---
+++ variables/creating-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Creating Variables
+ * Lanna Check
+ * 
+ * Learning how to create variables
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
