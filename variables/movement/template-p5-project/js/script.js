30d8db5250d755b4b1687dc5202a32e71c0e3eab
Lanna Check <lannacheck@gmail.com>
2025-09-15 02:37:35 -0400

    Added a model of velocity and acceleration to the bird

--- variables/movement/template-p5-project/js/script.js
+++ variables/movement/template-p5-project/js/script.js
@@ -1,24 +1,67 @@
 /**
- * Title of Project
- * Author Name
+ * Movement
+ * Lanna Check
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Experimenting with movement
  */
 
 "use strict";
 
+// Bird variables
+
+let bird = {
+    x: 120,
+    y: 450,
+    size: 50,
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    minVelocity: {
+        x: -3,
+        y: -2
+    },
+    maxVelocity: {
+        x: 3,
+        y: 2
+    },
+    accelaration: {
+        x: 0.025,
+        y: -0.2
+    }
+};
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a canvas
 */
 function setup() {
+    createCanvas(640, 480);
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Move a bird and display it
 */
 function draw() {
+    background(0);
+
+    // Move the bird
+    bird.velocity.x += bird.accelaration.x;
+    bird.velocity.y += bird.accelaration.y;
+
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+
+
+    bird.x += bird.velocity.x;
+    bird.y += bird.velocity.y;
+
+    // Draw the bird
+    push();
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(bird.x, bird.y, bird.size);
+    pop();
 
 }
