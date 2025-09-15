4dec129db8400502617f31a253b5a96b33fff779
Lanna Check <lannacheck@gmail.com>
2025-09-15 00:57:03 -0400

    Started the introducing variables project

---
+++ variables/introducing-variables/introducing-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing Variables
+ * Lanna check
+ * 
+ * Learning how to use variables
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
