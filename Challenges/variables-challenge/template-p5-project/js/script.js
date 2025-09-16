24f9d8244b5874b65b31e650e1a50d08af3f8ff8
Lanna Check <lannacheck@gmail.com>
2025-09-16 16:09:02 -0400

    Did step two of the variables challenge

--- Challenges/variables-challenge/template-p5-project/js/script.js
+++ Challenges/variables-challenge/template-p5-project/js/script.js
@@ -1,24 +1,55 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Mr. Furious
+ * Lanna Check
+ * Arielle Wong
+ * Chloe Guerin
+ *
+ * A guy who becomes visibly furious!
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+// Our friend Mr. Furious
+let mrFurious = {
+    // Position and size
+    x: 200,
+    y: 200,
+    size: 100,
+    // Colour
+    fill: {
+        r: 255,
+        g: 225,
+        b: 225
+    }
+};
 
+/**
+ * Create the canvas
+ */
+function setup() {
+    createCanvas(400, 400);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
+    background(160, 180, 200);
 
+    // Make Mr. Furious get redder over time
+
+    //Reduce the green value
+    mrFurious.fill.g -= 1;
+    mrFurious.fill.g = constrain(mrFurious.fill.g, 0, 225);
+
+    //Reduce the blue value
+    mrFurious.fill.b -= 1;
+    mrFurious.fill.b = constrain(mrFurious.fill.b, 0, 225);
+
+    // Draw Mr. Furious as a coloured circle
+    push();
+    noStroke();
+    fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+    ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+    pop();
 }
