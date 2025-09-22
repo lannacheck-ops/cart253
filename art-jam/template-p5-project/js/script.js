b5e6ac05be7c5b322b753884fa30f6ec1d74e226
Lanna Check <lannacheck@gmail.com>
2025-09-21 23:37:08 -0400

    Started the art jam
    My initial idea is to make cat-sorna of myself and players can use the mouse to interact with it to create different reactions.

---
+++ art-jam/template-p5-project/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * Art Jam
+ * Lanna Check
+ * 
+ * An interactive self-potrait where player pet my cat persona
+ */
+
+"use strict";
+
+
+
+/**
+ * Creates the canvas
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
