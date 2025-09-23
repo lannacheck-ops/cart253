10cef338e0e21be0c5ad3c99fe701dd99bd15f20
Lanna Check <lannacheck@gmail.com>
2025-09-23 15:36:23 -0400

    Made the puck move along the x and y axis
    We added two variables for the velocity on each axis and we added two variable for the direction on each axis. The direction is either a negative or a positive. The movement is represented through this equation : velocity * direction.

--- Challenges/conditionals-challenge/template-p5-project/js/script.js
+++ Challenges/conditionals-challenge/template-p5-project/js/script.js
@@ -1,24 +1,123 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Circle Master
+ * Lanna Check, Arielle Wong, 
+ *
+ * This will be a program in which the user can push a circle
+ * on the canvas using their own circle.
  */
 
-"use strict";
+const puck = {
+    x: 200,
+    y: 200,
+    size: 100,
+    fill: "#ff0000",
+    vX: 0,
+    vY: 0,
+    maxV: 5,
+    dirX: 1,
+    dirY: 1
+};
+
+const user = {
+    x: undefined, // will be mouseX
+    y: undefined, // will be mouseY
+    size: 75,
+    fill: "#000000"
+};
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Create the canvas
+ */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
+/**
+ * Move the user circle, check for overlap, draw the two circles
+ */
+function draw() {
+    background("#aaaaaa");
+
+    // Move user circle
+    moveUser();
+
+    // Draw the user and puck
+    drawUser();
+    drawPuck();
+    movePuck();
+
+    puck.x = constrain(puck.x, 0 + (puck.size / 2), width - (puck.size / 2));
+    puck.y = constrain(puck.y, 0 + (puck.size / 2), height - (puck.size / 2));
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
+ * Sets the user position to the mouse position
+ */
+function moveUser() {
+    user.x = mouseX;
+    user.y = mouseY;
+}
 
+/**
+ * Displays the user circle
+ */
+function drawUser() {
+    push();
+    noStroke();
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
+    pop();
+}
+
+/**
+ * Displays the puck circle
+ */
+function drawPuck() {
+    push();
+    noStroke();
+    fill(puck.fill);
+    ellipse(puck.x, puck.y, puck.size);
+    pop();
+}
+
+function movePuck() {
+
+    const distance = dist(user.x, user.y, puck.x, puck.y);
+    const checkOverlap = (distance < user.size / 2 + puck.size / 2);
+
+    const distanceX = dist(user.x, 0, puck.x, 0);
+    const distanceY = dist(0, user.y, 0, puck.y)
+
+
+    if (checkOverlap) {
+        if (distanceX < distanceY) {
+            puck.vX = puck.maxV
+            if (user.x > puck.x) {
+                puck.dirX = -1
+            }
+            else {
+                puck.dirX = 1
+            }
+        }
+        if (distanceY < distanceX) {
+            puck.vY = puck.maxV
+            if (user.y > puck.y) {
+                puck.dirY = -1
+            }
+            else {
+                puck.dirY = 1
+            }
+        }
+
+
+    }
+    else {
+        puck.vX -= 0.1;
+        puck.vY -= 0.1;
+        puck.vX = constrain(puck.vX, 0, puck.maxV);
+        puck.vY = constrain(puck.vY, 0, puck.maxV);
+    }
+
+    puck.x += puck.vX * puck.dirX;
+    puck.y += puck.vY * puck.dirY;
 }
