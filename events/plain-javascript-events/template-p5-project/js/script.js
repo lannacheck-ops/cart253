4c0505bbe67c927f8eef5defb40f6aab88051318
Lanna Check <lannacheck@gmail.com>
2025-10-01 10:24:14 -0400

    Started Plain JavaScript project

---
+++ events/plain-javascript-events/template-p5-project/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Plain JavaScript Events
+ * Lanna Check
+ * 
+ * Experimenting with event handling in Plain JavaScript
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
