6876ab32d87a45b8240947fe534d11e52b43c2af
Lanna Check <lannacheck@gmail.com>
2025-10-01 09:32:56 -0400

    Started Introducing events

---
+++ events/introducing-events/template-p5-project/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing events
+ * Lanna Check
+ * 
+ * Taking a look at how events work in JavaScript and p5
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(400, 400);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
