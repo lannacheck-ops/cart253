823536fc88c057005f18ca2a7fa6a4842bf80e7e
Lanna Check <lannacheck@gmail.com>
2025-10-01 09:35:47 -0400

    Made an event example

--- events/introducing-events/template-p5-project/js/script.js
+++ events/introducing-events/template-p5-project/js/script.js
@@ -8,16 +8,28 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Draws canvas and background
 */
 function setup() {
     createCanvas(400, 400);
+    background(0);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Does nothing
 */
 function draw() {
 
+}
+
+/**
+ * Draws a circle at the mouse location
+*/
+function mousePressed() {
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(mouseX, mouseY, 50);
+    pop();
 }
