1565f784fe47943232ae212b5a313660b85d01d6
Lanna Check <lannacheck@gmail.com>
2025-11-04 12:26:43 -0500

    Finished the gradient project

--- loops/gradient/template-p5-project/js/script.js
+++ loops/gradient/template-p5-project/js/script.js
@@ -2,23 +2,35 @@
  * Gradient
  * Lanna Check
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Draws a gradient out of lines
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a canvas
 */
 function setup() {
-
+    createCanvas(600, 300);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draw a gradient
 */
 function draw() {
+    background(0);
 
+    let y = 0;
+
+    randomSeed(0);
+    for (let x = 0; x <= width; x++) {
+        const shade = map(x, 0, width, 0, 255);
+        push();
+        stroke(shade);
+        line(x, y, x, height)
+        pop();
+
+        y += random(0, 2);
+    }
 }
