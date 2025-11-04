609e74f8106f86d983da2d451d3bb872f1f1a08b
Lanna Check <lannacheck@gmail.com>
2025-11-04 12:17:52 -0500

    Finished star field project

--- loops/star-field/template-p5-project/js/script.js
+++ loops/star-field/template-p5-project/js/script.js
@@ -2,23 +2,44 @@
  * Star Field
  * Lanna Check
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Draws a star field with a for loop
  */
 
 "use strict";
 
+const numStars = 100;
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create canvas
 */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the star-field
 */
 function draw() {
+    background(0);
 
+    randomSeed(1);
+    for (let i = 0; i < numStars; i++) {
+        drawStar();
+    }
+}
+
+/**
+ * Draws a random star
+ */
+function drawStar() {
+    const x = random(0, width);
+    const y = random(0, height);
+    const diameter = random(2, 5);
+
+    push();
+    fill(255);
+    noStroke();
+    ellipse(x, y, diameter);
+    pop();
 }
