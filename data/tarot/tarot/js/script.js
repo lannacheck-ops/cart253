667fa203f96bdf62b5e18a8ae69289b185803a0c
Lanna Check <lannacheck@gmail.com>
2025-11-11 02:22:17 -0500

    Finished tarot project

--- data/tarot/tarot/js/script.js
+++ data/tarot/tarot/js/script.js
@@ -7,17 +7,44 @@
 
 "use strict";
 
+// Our tarot data
+let tarot = undefined;
+
+let fortune = "Click to show a fortune.";
+
 /**
- * tbd.
+ * Load tarot data
+ */
+function preload() {
+    tarot = loadJSON("assets/data/tarot_interpretations.json");
+}
+
+/**
+ * Create Canvas
 */
 function setup() {
-
+    createCanvas(800, 400);
 }
 
 
 /**
- * tbd.
+ * Display tarot
 */
 function draw() {
+    background(0);
 
+    // Display the information 
+    push();
+    textSize(16);
+    fill("yellow");
+    textAlign(CENTER, CENTER);
+    text(fortune, width / 2, height / 2);
+    pop();
+}
+
+function mousePressed() {
+    // Choose random card
+    const card = random(tarot.tarot_interpretations);
+    // Read the fortune from the random card
+    fortune = card.fortune_telling;
 }
