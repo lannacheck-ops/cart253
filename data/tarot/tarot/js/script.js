9408ea50f17b785628a75bfcdefd69e6a6e7ae02
Lanna Check <lannacheck@gmail.com>
2025-11-11 02:04:37 -0500

    Started the tarot project

---
+++ data/tarot/tarot/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Tarot
+ * Lanna Check
+ * 
+ * Some experiments with data representing a Tarot deck
+ */
+
+"use strict";
+
+/**
+ * tbd.
+*/
+function setup() {
+
+}
+
+
+/**
+ * tbd.
+*/
+function draw() {
+
+}
