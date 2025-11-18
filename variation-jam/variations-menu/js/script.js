be2aef2b4cb25466b01fbd5c4d40f99eb539abc6
Lanna Check <lannacheck@gmail.com>
2025-11-18 13:07:55 -0500

    Started the variation jam using the variations menu as a base code

---
+++ variation-jam/variations-menu/js/script.js
@@ -0,0 +1,83 @@
+/**
+ * Variation Menu
+ * Pippin Barr
+ * 
+ * A relatively simple example of a set of variations within a single
+ * project. (When we learn Object-Oriented Programming this will be a
+ * LOT easier.)
+ */
+
+"use strict";
+
+let state = "menu";
+
+/**
+ * Create the canvas
+*/
+function setup() {
+    createCanvas(500, 500);
+}
+
+
+/**
+ * Display the menu or the current variation
+*/
+function draw() {
+    // A "switch" statement is like a cleaner alternative to writing multiple if...else if...else statements.
+    switch (state) { // Check value in the "state" variable
+        case "menu": // Compare if the state value is equal to "menu"
+            menuDraw(); // Call menuDraw function if the state is "menu"
+            break;//Stops JavaScript from continuing to check the other cases.
+        case "red-variation":
+            redDraw();
+            break
+        case "green-variation":
+            greenDraw();
+            break;
+        case "blue-variation":
+            blueDraw();
+            break;
+    }
+}
+
+/**
+ * Listen for mouse pressed and call the function for it in the
+ * current state
+ */
+function mousePressed() {
+    switch (state) {
+        case "menu":
+            menuMousePressed();
+            break;
+        case "red-variation":
+            redMousePressed();
+            break
+        case "green-variation":
+            greenMousePressed();
+            break;
+        case "blue-variation":
+            blueMousePressed();
+            break;
+    }
+}
+
+/**
+ * Listen for keypressed and call the function for it in the
+ * current state
+ */
+function keyPressed(event) {
+    switch (state) {
+        case "menu":
+            menuKeyPressed(event);
+            break;
+        case "red-variation":
+            redKeyPressed(event);
+            break
+        case "green-variation":
+            greenKeyPressed(event);
+            break;
+        case "blue-variation":
+            blueKeyPressed(event);
+            break;
+    }
+}
